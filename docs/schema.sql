bb62af72a7bb0197915050e9259fcc2b1483f89f
Levi Halvorson <lhalvorson34@gmail.com>
2020-10-25 09:07:47 -0600

    added more info to csv files

--- docs/schema.sql
+++ docs/schema.sql
@@ -1,20 +1,20 @@
 -- create database employee_db;
 use employee_db;
 
-CREATE TABLE department (
-  id INT NOT NULL primary key,
-  empName VARCHAR(30) NULL
-);
+-- CREATE TABLE department (
+--   id INT NOT NULL primary key,
+--   name VARCHAR(30) NULL
+-- );
 
 CREATE TABLE empRole (
-	id INT NOT NULL primary key,
+	id INT NOT NULL auto_increment primary key,
 	title VARCHAR(30) NULL,
 	salary VARCHAR(30) NULL,
 	department_id VARCHAR(30) NULL
 );
 
 CREATE TABLE employee (
-	id INT NOT NULL primary key,
+	id INT NOT NULL auto_increment primary key,
 	first_name VARCHAR(30) NULL,
 	last_name VARCHAR(30) NULL,
 	role_id INT NULL,
